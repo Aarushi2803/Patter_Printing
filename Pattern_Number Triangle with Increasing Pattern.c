3e30e71f438160e7085df60ee68fbed000d140e9
Aarushi <aarushimandloi744@gmail.com>
2025-02-23 21:22:38 +0530

    dec-inc patter

---
+++ Pattern_Number Triangle with Increasing Pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int n;
+scanf("%d",&n);
+ for(int i=1;i<=n;i++){
+     int num =i;
+    for(int j=1;j<=i;j++){
+        printf("%d ",num);
+        num+=(n-j);
+    }
+     printf("\n");
+ }    
+    return 0;
+}
